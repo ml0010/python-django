2fd6fbe44dd99c19213874d70b0bc9758efd7cca
Mimi L <lim.mimic@gmail.com>
2026-07-02 16:11:22 +0200

    add: django-postgres migration

--- storefront/settings.py
+++ storefront/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = "storefront.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "storefront",
+        "HOST": "localhost",
+        "USER": "mimi",
+        "PASSWORD": "",
+        "PORT": "5432"
     }
 }
 
