704ba5e11d500590709a7b3bb8933c40f5fa254a
Mimi L <lim.mimic@gmail.com>
2026-07-02 14:22:13 +0200

    add: migrations

--- store/models.py
+++ store/models.py
@@ -14,8 +14,9 @@ class Product(models.Model):
    # id field is not required as django will add automatically
    # sku = models.CharField(max_length=10, primary_key=True)
    title = models.CharField(max_length=255)
+   slug = models.SlugField()
    description = models.TextField()
-   price = models.DecimalField(max_digits=6, decimal_places=2)
+   unit_price = models.DecimalField(max_digits=6, decimal_places=2)
    inventory = models.IntegerField()
    last_update = models.DateTimeField(auto_now=True)
    collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
@@ -61,7 +62,7 @@ class Address(models.Model):
    street = models.CharField(max_length=255)
    city = models.CharField(max_length=255)
    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
-
+   zip = models.CharField(max_length=255, null=True)
 
 class Cart(models.Model):
    created_at = models.DateTimeField(auto_now_add=True)
