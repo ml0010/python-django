dd8ff7939ffb9efffc5c58a3b12c0730cc596cc0
Mimi L <lim.mimic@gmail.com>
2026-07-07 17:03:41 +0200

    edit: django models"

--- playground/views.py
+++ playground/views.py
@@ -1,25 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from store.models import Product
-from django.core.exceptions import ObjectDoesNotExist
-# Create your views here.
-# requrst -> response
+from django.db.models import Q, F
+from store.models import Product, OrderItem, Order, Customer
+from django.db.models import Count, Max, Min, Avg, Sum
+from django.db.models import Value, F, Func, Count, ExpressionWrapper, DecimalField
+from django.db.models.functions import Concat
 
-
-# Pull data from db
-# Transform
-# Send email...
+from django.contrib.contenttypes.models import ContentType
+from store.models import Product, Collection
 
 
 def say_hello(request):
-   #queryset = Product.objects.filter(unit_price__range=(20, 30))
-   #queryset = Product.objects.filter(collection__id__range=(1, 2, 3))
-   #queryset = Product.objects.filter(title__icontains='coffee')
-   #queryset = Product.objects.filter(last_update__year=2021)
-   queryset = Product.objects.filter(description__isnull=True)
 
-   return render(request, 'hello.html', {'name': 'Mimi', 'products': list(queryset)})
+
+   return render(request, 'hello.html', {'name': 'Mimi'})
    
-   #return HttpResponse('Hello World')
-
+ 
    
--- store/models.py
+++ store/models.py
@@ -9,6 +9,11 @@ class Promotion(models.Model):
 class Collection(models.Model):
    title = models.CharField(max_length=255)
    featured_product = models.ForeignKey('Product', on_delete=models.SET_NULL, null=True, related_name='+')
+   def __str__(self):
+      return self.title
+   
+   class Meta:
+      ordering = ['title']
 
 class Product(models.Model):
    # id field is not required as django will add automatically
@@ -21,6 +26,12 @@ class Product(models.Model):
    last_update = models.DateTimeField(auto_now=True)
    collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
    promotions = models.ManyToManyField(Promotion)
+
+   def __str__(self):
+      return self.title
+   
+   class Meta:
+      ordering = ['title']
 
 class Customer(models.Model):
    # Human readable name is used in dropdown menu
@@ -38,6 +49,12 @@ class Customer(models.Model):
    phone = models.CharField(max_length=255)
    birth_date = models.DateField(null=True)
    membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
+
+   def __str__(self):
+      return f'{self.first_name} {self.last_name}'
+   
+   class Meta:
+      ordering = ['first_name', 'last_name']
 
 class Order(models.Model):
    PAYMENT_STATUS_PENDING = 'P'
